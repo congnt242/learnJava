73ac305f8f29c4b25cd3a03334e381a1fd319786
Cong Nguyen <congnt242@gmail.com>
2020-12-01 15:38:48 +0700

    create repository to learn java programming language.

---
+++ src/test/Greeting.java
@@ -0,0 +1,15 @@
+package test;
+import java.util.Scanner;
+
+public class Greeting {
+    Scanner userName = new Scanner(System.in);
+    void userGreeting() {
+        String user;
+        String userNameUppercase;
+        System.out.print("\nWhat 's your name?");
+        user = userName.nextLine();
+        userNameUppercase = user.toUpperCase();
+        System.out.print("\nHello "+userNameUppercase+", nice to meet you.");
+        
+    }
+}
---
+++ src/test/main.java
@@ -0,0 +1,25 @@
+package test;
+
+public class main {
+	public static void main(String Args[]) {
+	    Greeting user = new Greeting();
+	    user.userGreeting();
+	}
+}
+
+class second{
+    public int roolAdice() {
+        int firstDice = 0;
+        int secondDice = 0;
+        int totalRoll = 0;
+        
+        firstDice = (int)(Math.random()*6) + 1;
+        secondDice = (int)(Math.random()*6) + 1;
+        totalRoll = firstDice + secondDice;
+        
+        System.out.printf("The first dice comes up %d",firstDice);
+        System.out.printf("\nThe second dice comes up %d",secondDice);
+        System.out.printf("\nYour total roll is %d",totalRoll);
+        return 1;
+    }
+}
